03c686eb0a542c683ea60de5b9349f2e21e34a1b
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-11-18 17:28:41 -0800

    integrated xp share logic

--- src/main/kotlin/us/timinc/mc/cobblemon/capturexp/CaptureXP.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/capturexp/CaptureXP.kt
@@ -4,6 +4,7 @@ import com.cobblemon.mod.common.Cobblemon
 import com.cobblemon.mod.common.api.events.CobblemonEvents
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent
 import com.cobblemon.mod.common.api.pokemon.experience.SidemodExperienceSource
+import com.cobblemon.mod.common.api.tags.CobblemonItemTags
 import com.cobblemon.mod.common.battles.pokemon.BattlePokemon
 import com.cobblemon.mod.common.util.isInBattle
 import me.shedaniel.autoconfig.AutoConfig
@@ -29,12 +30,21 @@ object CaptureXP : ModInitializer {
     private fun handleCaptureInBattle(event: PokemonCapturedEvent) {
         val battle = Cobblemon.battleRegistry.getBattleByParticipatingPlayer(event.player) ?: return
         val caughtBattleMonActor = battle.actors.find { it.uuid == event.pokemon.uuid }!!
-        caughtBattleMonActor.pokemonList.forEach { caughtBattleMon ->
-            caughtBattleMon.facedOpponents.filter { opponent -> opponent.health > 0 }.forEach { opponent ->
-                val experience =
-                    Cobblemon.experienceCalculator.calculate(opponent, caughtBattleMon, captureXPConfig.multiplier)
+        val caughtBattleMon = caughtBattleMonActor.pokemonList.find { it.uuid == event.pokemon.uuid }!!
+
+        caughtBattleMonActor.getSide().getOppositeSide().actors.forEach { opponentActor ->
+            opponentActor.pokemonList.filter {
+                it.health > 0 && (caughtBattleMon.facedOpponents.contains(it) || it.effectedPokemon.heldItem()
+                    .`is`(CobblemonItemTags.EXPERIENCE_SHARE))
+            }.forEach { opponentMon ->
+                val xpShareOnly = !caughtBattleMon.facedOpponents.contains(opponentMon)
+                val xpShareOnlyModifier =
+                    (if (xpShareOnly) Cobblemon.config.experienceShareMultiplier else 1).toDouble()
+                val experience = Cobblemon.experienceCalculator.calculate(
+                    opponentMon, caughtBattleMon, captureXPConfig.multiplier * xpShareOnlyModifier
+                )
                 if (experience > 0) {
-                    opponent.actor.awardExperience(opponent, experience)
+                    opponentActor.awardExperience(opponentMon, experience)
                 }
             }
         }
@@ -43,10 +53,20 @@ object CaptureXP : ModInitializer {
     private fun handleCaptureOutOfBattle(event: PokemonCapturedEvent) {
         val playerParty = Cobblemon.storage.getParty(event.player)
         val source = SidemodExperienceSource(MOD_ID)
-        val targetPokemon = playerParty.firstOrNull { it != event.pokemon && it.currentHealth > 0 } ?: return
-        val experience = Cobblemon.experienceCalculator.calculate(
-            BattlePokemon.safeCopyOf(targetPokemon), BattlePokemon.safeCopyOf(event.pokemon), captureXPConfig.multiplier
-        )
-        targetPokemon.addExperienceWithPlayer(event.player, source, experience)
+        val first = playerParty.firstOrNull { it != event.pokemon && it.currentHealth > 0 } ?: return
+        val targetMons = playerParty.filter {
+            it != event.pokemon && it.currentHealth > 0 && (it.uuid == first.uuid || it.heldItem()
+                .`is`(CobblemonItemTags.EXPERIENCE_SHARE))
+        }
+        targetMons.forEach { targetMon ->
+            val xpShareOnly = targetMon.uuid != first.uuid
+            val xpShareOnlyModifier = (if (xpShareOnly) Cobblemon.config.experienceShareMultiplier else 1).toDouble()
+            val experience = Cobblemon.experienceCalculator.calculate(
+                BattlePokemon.safeCopyOf(targetMon),
+                BattlePokemon.safeCopyOf(event.pokemon),
+                captureXPConfig.multiplier * xpShareOnlyModifier
+            )
+            targetMon.addExperienceWithPlayer(event.player, source, experience)
+        }
     }
 }
